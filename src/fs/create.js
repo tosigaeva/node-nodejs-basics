2a95effc314e96d19587d2989b344eb1c04d2938
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-04-23 12:18:38 +0200

    feat(fs): implement create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    try {
+      await fs.writeFile('src/fs/files/fresh.txt', 'I am fresh and young', {
+        encoding: 'utf8',
+        flag: 'wx'
+      });
+      console.log('fresh.txt was created successfully');
+    } catch (error) {
+      if (error.code === 'EEXIST') {
+        throw new Error('FS operation failed');
+      }
+      throw error;
+    }
 };
 
 await create();
