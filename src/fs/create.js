9807a47fa87040c7c608e6b7d60a84695b2844a3
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-04-23 17:56:20 +0200

    fix(fs): use universal file path

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,8 +1,14 @@
 import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const create = async () => {
+  const filePath = path.join(__dirname, 'files', 'fresh.txt');
     try {
-      await fs.writeFile('src/fs/files/fresh.txt', 'I am fresh and young', {
+      await fs.writeFile(filePath, 'I am fresh and young', {
         encoding: 'utf8',
         flag: 'wx'
       });
