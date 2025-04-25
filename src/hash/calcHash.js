fb31f20e95043a74c3294f4411c8e7a75e0dd5d3
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-04-25 22:30:47 +0200

    feat(hash): implement calcHash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import {createHash} from 'crypto';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+   const sourcePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+   const stream = fs.createReadStream(sourcePath);
+   const hash = createHash('sha256');
+
+  stream.on('data', (chunk) => hash.update(chunk));
+  stream.on('end', () => console.log(hash.digest('hex')));
+  stream.on('error', (error) => console.error(error.message));
 };
 
 await calculateHash();
