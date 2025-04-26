dae6fbc5606e667c364b93ba95bcb87220917dd9
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-04-26 23:26:20 +0200

    feat(streams): implement transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import {Transform} from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      try {
+        this.push(chunk.toString().split('').reverse().join(''));
+        callback();
+      } catch (error) {
+        callback(error);
+      }
+    }
+  });
+
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+  process.stdin.on('error', (error) => console.error(error.message));
+  reverseTransform.on('error', (error) => console.error(error.message));
+  process.stdout.on('error', (error) => console.error(error.message));
 };
 
 await transform();
