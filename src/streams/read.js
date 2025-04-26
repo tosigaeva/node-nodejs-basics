aac74a4fa4837b4e7d49e3786dff57981c6ae1ca
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-04-26 18:11:34 +0200

    feat(streams): implement read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const sourcePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  const stream = fs.createReadStream(sourcePath, {encoding: 'utf-8'});
+
+  stream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  })
+
+  stream.on('error', (err) => {
+    console.error(err.message);
+  });
 };
 
 await read();
