42dbe8956e36ad83d510f180a6e11e9d4649a5c9
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-04-26 19:21:03 +0200

    feat(streams): implement write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const sourcePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+  const writeStream = fs.createWriteStream(sourcePath);
+
+  process.stdin.pipe(writeStream);
+
+  process.stdin.on('error', (error) => {
+    console.error(error.message);
+  });
+
+  writeStream.on('error', (error) => {
+    console.error(error.message);
+  })
 };
 
 await write();
